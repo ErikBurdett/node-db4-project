7a8af5b13525ffd60b1baf52f189d7da9b51d213
ErikBurdett <erikaburdet@gmail.com>
2021-06-10 19:11:43 -0500

    as far as i can get today

---
+++ data/seeds/02-make-recipes.js
@@ -0,0 +1,18 @@
+const recipes = [
+    { recipe_name: 'Chili Ramen'},
+    { recipe_name: 'Habenero Mango Michelada'},
+    { recipe_name: 'Smokey Jalapeno Biscuits & Gravy'}
+]
+
+
+const ingredients = [
+    { ingredient_name: 'Peppers', ingredient_unit:"grams" },
+    { ingredient_name: 'Peppers',ingredient_unit: "grams"},
+    { ingredient_name: 'Peppers', ingredient_unit:"grams"},
+    { ingredient_name: 'Peppers', ingredient_unit:"grams"},
+    { ingredient_name: 'Peppers', ingredient_unit:"grams"},
+    { ingredient_name: 'Peppers',ingredient_unit:"grams" }
+]
+exports.seed = function(knex){
+
+}
